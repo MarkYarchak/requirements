8d3b3016c3f31b37c52b5a49bc04c8beb7faa5e7
Mark Yarchak <markyarchak@gmail.com>
2020-05-24 22:24:16 +0300

    added requirements

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  router: {
+    base: '/dist/'
+  },
   /*
   ** Customize the progress-bar color
   */
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,310 @@
 <template>
   <v-layout
-    column
     justify-center
-    align-center
+    wrap
   >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
+    <div class="pa-3 full-width-block card-center-height">
+      <v-card
+        max-width="400"
+        width="100%"
+      >
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          WEB development
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
+          <v-list dense>
+            <v-list-item
+              v-for="(req, reqI) in webRequirementsList"
+              :key="reqI"
+              dense
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
+              {{ req }}
+            </v-list-item>
+          </v-list>
         </v-card-text>
+        <hr class="mb-2">
         <v-card-actions>
           <v-spacer />
           <v-btn
             color="primary"
             nuxt
-            to="/inspire"
+            @click="agreeWithRequirementsDialog = true"
           >
-            Continue
+            Agree
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </div>
+
+    <div class="pa-3 full-width-block card-center-height">
+      <v-card
+        max-width="400"
+        width="100%"
+      >
+        <v-card-title class="headline">
+          Browser and dev-tools
+        </v-card-title>
+        <v-card-text>
+          <v-list dense>
+            <v-list-item
+              v-for="(req, reqI) in browserRequirements"
+              :key="reqI"
+              dense
+            >
+              {{ req }}
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <hr class="mb-2">
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            @click="agreeWithRequirementsDialog = true"
+          >
+            Agree
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </div>
+
+    <div class="pa-3 full-width-block card-center-height">
+      <v-card
+        max-width="400"
+        width="100%"
+      >
+        <v-card-title class="headline">
+          Technologies and databases
+        </v-card-title>
+        <v-card-text>
+          <v-list dense>
+            <v-list-item
+              v-for="(req, reqI) in technologiesRequirementsList"
+              :key="reqI"
+              dense
+            >
+              {{ req }}
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <hr class="mb-2">
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            @click="agreeWithRequirementsDialog = true"
+          >
+            Agree
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </div>
+
+    <div class="pa-3 full-width-block card-center-height">
+      <v-card
+        max-width="400"
+        width="100%"
+      >
+        <v-card-title class="headline">
+          Protocols requirements
+        </v-card-title>
+        <v-card-text>
+          <v-list dense>
+            <v-list-item
+              v-for="(req, reqI) in networkAndProtocolsList"
+              :key="reqI"
+              dense
+            >
+              {{ req }}
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <hr class="mb-2">
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            @click="agreeWithRequirementsDialog = true"
+          >
+            Agree
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </div>
+
+    <div class="pa-3 full-width-block card-center-height">
+      <v-card
+        max-width="400"
+        width="100%"
+      >
+        <v-card-title class="headline">
+          Mobile development
+        </v-card-title>
+        <v-card-text>
+          <v-list dense>
+            <v-list-item
+              v-for="(req, reqI) in mobileRequirementsList"
+              :key="reqI"
+              dense
+            >
+              {{ req }}
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <hr class="mb-2">
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            @click="agreeWithRequirementsDialog = true"
+          >
+            Agree
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </div>
+
+    <div class="pa-3 full-width-block card-center-height">
+      <v-card
+        max-width="400"
+        width="100%"
+      >
+        <v-card-title class="headline">
+          Desktop and plugins
+        </v-card-title>
+        <v-card-text>
+          <v-list dense>
+            <v-list-item
+              v-for="(req, reqI) in desktopRequirementsList"
+              :key="reqI"
+              dense
+            >
+              {{ req }}
+            </v-list-item>
+          </v-list>
+        </v-card-text>
+        <hr class="mb-2">
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            @click="agreeWithRequirementsDialog = true"
+          >
+            Agree
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
+      </v-card>
+    </div>
+
+    <v-dialog
+      v-model="agreeWithRequirementsDialog"
+      width="300"
+    >
+      <v-card>
+        <v-card-title>Thanks</v-card-title>
+        <v-card-text>Thank you for requirements agreement</v-card-text>
+        <v-card-actions>
+          <v-btn
+            text
+            block
+            @click="agreeWithRequirementsDialog = false"
+          >
+            Close
           </v-btn>
         </v-card-actions>
       </v-card>
-    </v-flex>
+    </v-dialog>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+  data() {
+    return {
+      agreeWithRequirementsDialog: false,
+      webRequirementsList: [
+        'NodeJS',
+        'TypeScript',
+        'Express',
+        'Passport',
+        'Vue',
+        'Angular',
+        'NuxtJS',
+        'SocketIO',
+        'NestJS',
+        'Apollo',
+        'RxJS',
+        'Mongoose',
+        'Lodash',
+        'CSS preprocessors'
+      ],
+      mobileRequirementsList: [
+        'Kotlin',
+        'NativeScript',
+        'Dart',
+        'Flutter',
+      ],
+      desktopRequirementsList: [
+        'Electron',
+        'C# .NET',
+        'Python',
+        'C++',
+      ],
+      technologiesRequirementsList: [
+        'MongoDB',
+        'MySQL',
+        'GraphQL',
+        'SOLID',
+        'Material Design',
+        'Asynchronous programming',
+        'Object-oriented programming',
+        'Functional programming',
+        'Lint',
+        'Git',
+      ],
+      networkAndProtocolsList: [
+        'tcp/ip',
+        'http(s)',
+        'ws(s)',
+        'smtp',
+        'WebRTC',
+        'hls',
+        'ftp',
+      ],
+      browserRequirements: [
+        'Network',
+        'Console',
+        'Inspector (and debugger)',
+        'Service Workers',
+        'Local storage',
+        'Session storage',
+        'Cookies',
+        'IndexedDB',
+        'Load page performance (process)'
+      ],
+    };
+  },
 }
 </script>
+
+<style scoped>
+  .card-center-height {
+    height: calc(100% - 118px);
+  }
+  .full-width-block {
+    display: flex;
+    width: 100%;
+    max-width: 440px;
+  }
+</style>
