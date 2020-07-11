8048666222f0a1fd1c4347fa39f78ebd74471576
Mark Yarchak <markyarchak.work@gmail.com>
2020-07-12 01:39:28 +0300

    modular with new styles

---
+++ assets/requirementsList.js
@@ -0,0 +1,337 @@
+export default {
+  data() {
+    return {
+      requirementsList: [
+        {
+          title: 'WEB development',
+          list: [
+            {
+              title: 'NodeJS',
+              description: 'Node.js is a JavaScript runtime built on Chrome\'s V8 JavaScript engine. \n\nhttps://nodejs.org/en/docs/',
+            },
+            {
+              title: 'TypeScript',
+              description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript. \n\nWorking directly in Deno',
+            },
+            {
+              title: 'Express',
+              description: '',
+            },
+            {
+              title: 'Passport',
+              description: '',
+            },
+            {
+              title: 'Vue',
+              description: '',
+            },
+            {
+              title: 'Angular',
+              description: '',
+            },
+            {
+              title: 'NuxtJS',
+              description: '',
+            },
+            {
+              title: 'SocketIO',
+              description: '',
+            },
+            {
+              title: 'NestJS',
+              description: '',
+            },
+            {
+              title: 'Apollo',
+              description: '',
+            },
+            {
+              title: 'RxJS',
+              description: '',
+            },
+            {
+              title: 'Mongoose',
+              description: '',
+            },
+            {
+              title: 'Lodash',
+              description: '',
+            },
+            {
+              title: 'React',
+              description: '',
+            },
+            {
+              title: 'CSS preprocessors',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Browser and dev-tools',
+          list: [
+            {
+              title: 'Network',
+              description: '',
+            },
+            {
+              title: 'Console',
+              description: '',
+            },
+            {
+              title: 'Inspector (and debugger)',
+              description: '',
+            },
+            {
+              title: 'Service Workers',
+              description: '',
+            },
+            {
+              title: 'Local storage',
+              description: '',
+            },
+            {
+              title: 'Session storage',
+              description: '',
+            },
+            {
+              title: 'Cookies',
+              description: '',
+            },
+            {
+              title: 'IndexedDB',
+              description: '',
+            },
+            {
+              title: 'Load page performance (process)',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Auth',
+          list: [
+            {
+              title: 'Authorization / Authentication',
+              description: '',
+            },
+            {
+              title: 'Validation',
+              description: '',
+            },
+            {
+              title: 'Token',
+              description: '',
+            },
+            {
+              title: 'OAuth2',
+              description: '',
+            },
+            {
+              title: 'Authorization header',
+              description: '',
+            },
+            {
+              title: 'JsonWebToken',
+              description: '',
+            },
+            {
+              title: 'Access and refresh tokens',
+              description: '',
+            },
+            {
+              title: 'Passport (JS etc.)',
+              description: '',
+            },
+            {
+              title: 'Bcrypt',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Server security',
+          list: [
+            {
+              title: 'Production mode (no dev configurations)',
+              description: '',
+            },
+            {
+              title: 'TLS',
+              description: '',
+            },
+            {
+              title: 'Cors',
+              description: '',
+            },
+            {
+              title: 'Helmet + Disabled X-Powered-By header',
+              description: '',
+            },
+            {
+              title: 'CSRF',
+              description: '',
+            },
+            {
+              title: 'Rate limit',
+              description: '',
+            },
+            {
+              title: 'Virtual isolated environments / Containers',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Technologies and databases',
+          list: [
+            {
+              title: 'MongoDB',
+              description: '',
+            },
+            {
+              title: 'MySQL',
+              description: '',
+            },
+            {
+              title: 'GraphQL',
+              description: '',
+            },
+            {
+              title: 'Shell/bash',
+              description: '',
+            },
+            {
+              title: 'File systems',
+              description: '',
+            },
+            {
+              title: 'Git',
+              description: '',
+            },
+            {
+              title: 'Docker',
+              description: '',
+            },
+            {
+              title: 'Kubernetes',
+              description: '',
+            },
+            {
+              title: 'Unit/e2e testing',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Protocols requirements',
+          list: [
+            {
+              title: 'tcp/ip',
+              description: '',
+            },
+            {
+              title: 'http(s)',
+              description: '',
+            },
+            {
+              title: 'http/2',
+              description: '',
+            },
+            {
+              title: 'ws(s)',
+              description: '',
+            },
+            {
+              title: 'smtp',
+              description: '',
+            },
+            {
+              title: 'WebRTC',
+              description: '',
+            },
+            {
+              title: 'hls',
+              description: '',
+            },
+            {
+              title: 'ftp',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Principles and rules',
+          list: [
+            {
+              title: 'SOLID',
+              description: '',
+            },
+            {
+              title: 'Material Design',
+              description: '',
+            },
+            {
+              title: 'Asynchronous programming',
+              description: '',
+            },
+            {
+              title: 'Object-oriented programming',
+              description: '',
+            },
+            {
+              title: 'Functional programming',
+              description: '',
+            },
+            {
+              title: 'Lint',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Mobile development',
+          list: [
+            {
+              title: 'Kotlin',
+              description: '',
+            },
+            {
+              title: 'NativeScript',
+              description: '',
+            },
+            {
+              title: 'Dart',
+              description: '',
+            },
+            {
+              title: 'Flutter',
+              description: '',
+            },
+          ],
+        },
+        {
+          title: 'Desktop and plugins',
+          list: [
+            {
+              title: 'Electron',
+              description: '',
+            },
+            {
+              title: 'C# .NET',
+              description: '',
+            },
+            {
+              title: 'Python',
+              description: '',
+            },
+            {
+              title: 'C/C++',
+              description: '',
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
---
+++ components/OneRequirementDescription.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-html="descriptionContent"></div>
+</template>
+
+<script>
+import MarkdownIt from 'markdown-it';
+
+export default {
+  name: 'OneRequirementDescription',
+  props: {
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      md: new MarkdownIt({
+        breaks: true,
+        linkify: true,
+        typographer: true,
+      }),
+      agreeWithRequirementsDialog: false,
+    };
+  },
+  computed: {
+    descriptionContent() {
+      return this.md.render(this.description || 'No description');
+    },
+  },
+};
+</script>
+
+<style lang="" scoped>
+
+</style>
